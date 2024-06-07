8ce34eddf438c4639d30b767522f5db261f1d3a4
RamizSaud <ramizsaud15@gmail.com>
2024-06-07 12:25:55 +0500

    BreakOut Game

---
+++ build/libs/game/src/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/DuneAI/Game/libs/game/src/ball_object.cpp" "libs/game/src/CMakeFiles/game.dir/ball_object.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/ball_object.cpp.obj.d"
+  "D:/DuneAI/Game/libs/game/src/ball_object_collisions.cpp" "libs/game/src/CMakeFiles/game.dir/ball_object_collisions.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/ball_object_collisions.cpp.obj.d"
+  "D:/DuneAI/Game/libs/game/src/game.cpp" "libs/game/src/CMakeFiles/game.dir/game.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/game.cpp.obj.d"
+  "D:/DuneAI/Game/libs/game/src/game_level.cpp" "libs/game/src/CMakeFiles/game.dir/game_level.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/game_level.cpp.obj.d"
+  "D:/DuneAI/Game/libs/game/src/game_object.cpp" "libs/game/src/CMakeFiles/game.dir/game_object.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/game_object.cpp.obj.d"
+  "D:/DuneAI/Game/libs/game/src/sprite_renderer.cpp" "libs/game/src/CMakeFiles/game.dir/sprite_renderer.cpp.obj" "gcc" "libs/game/src/CMakeFiles/game.dir/sprite_renderer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/libs/game/src/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/ball_object.cpp.obj"
+  "CMakeFiles/game.dir/ball_object.cpp.obj.d"
+  "CMakeFiles/game.dir/ball_object_collisions.cpp.obj"
+  "CMakeFiles/game.dir/ball_object_collisions.cpp.obj.d"
+  "CMakeFiles/game.dir/game.cpp.obj"
+  "CMakeFiles/game.dir/game.cpp.obj.d"
+  "CMakeFiles/game.dir/game_level.cpp.obj"
+  "CMakeFiles/game.dir/game_level.cpp.obj.d"
+  "CMakeFiles/game.dir/game_object.cpp.obj"
+  "CMakeFiles/game.dir/game_object.cpp.obj.d"
+  "CMakeFiles/game.dir/sprite_renderer.cpp.obj"
+  "CMakeFiles/game.dir/sprite_renderer.cpp.obj.d"
+  "libgame.a"
+  "libgame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
